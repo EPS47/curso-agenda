9cb25fd8e66bf561084b4735fb15ace681c16a65
Eliabe <eliabesilva.p@gmail.com>
2024-02-25 21:47:44 -0300

    update final

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -5,12 +6,14 @@ from django.utils import timezone
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 # category (foreign key), show (boolean), picture (imagem)
-
-# Depois
 # owner (foreign key)
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -25,10 +28,17 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    picture = models.ImageField(blank=True, upload_to="pictures/%Y/%m/")
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, blank=True, null=True
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
     )
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
